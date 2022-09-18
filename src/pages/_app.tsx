be961161fc48810f6d75756cd3a6985b1cc337b4
Jakub Wilk <jw@jakubwilk.pl>
2022-09-18 22:24:51 +0200

    Setup tailwind css and mantine ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { MantineProvider } from '@mantine/core'
+import { Fragment } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <meta name={'viewport'} content={'minimum-scale=1, initial-scale=1, width=device-width'} />
+      </Head>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </Fragment>
+  )
 }
 
 export default MyApp
