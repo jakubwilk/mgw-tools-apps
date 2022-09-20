e55aa8502150e584d620e1b69698b65c844c18b3
Jakub Wilk <jw@jakubwilk.pl>
2022-09-20 20:41:15 +0200

    Create data loader component

---
+++ src/components/common/ui/DataLoader.tsx
@@ -0,0 +1,11 @@
+import { Loader } from '@mantine/core'
+
+function DataLoader() {
+  return (
+    <div className={'w-full h-full flex justify-center items-center'}>
+      <Loader size={'xl'} />
+    </div>
+  )
+}
+
+export default DataLoader
