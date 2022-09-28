8ab7f25c3891d10e9621d39a9f0a792b0c3b65b7
Jakub Wilk <jw@jakubwilk.pl>
2022-09-28 22:13:21 +0200

    Create calculator subpage

---
+++ src/components/calculator/page/CalculatorPage.tsx
@@ -0,0 +1,36 @@
+import Layout from '../../common/ui/Layout'
+import Link from 'next/link'
+import { Button, clsx, createStyles, Tooltip } from '@mantine/core'
+
+const useStyles = createStyles((theme) => ({
+  link: {
+    color: theme.colors.mgw[9],
+    transition: 'all .1s ease-in-out',
+
+    '&:hover, &:focus': {
+      color: 'white',
+      backgroundColor: theme.colors.mgw,
+    },
+  },
+}))
+
+function CalculatorPage() {
+  const { classes } = useStyles()
+
+  return (
+    <Layout>
+      <div className={clsx('h-full', 'flex flex-col justify-between items-center')}>
+        <p>{'Kalkulator'}</p>
+        <Link href={'/'} passHref>
+          <Tooltip label={'Powrót do strony głównej'} withArrow>
+            <Button component={'a'} className={clsx(classes.link, 'px-4 py-2')}>
+              {'Wstecz'}
+            </Button>
+          </Tooltip>
+        </Link>
+      </div>
+    </Layout>
+  )
+}
+
+export default CalculatorPage
